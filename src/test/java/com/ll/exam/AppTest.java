413ce11a9450242179b74c547fc1a73752df6f3b
woominsik <9737638@naver.com>
2022-07-13 13:16:35 +0900

    테스트 데이터 리다이렉팅

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,4 +1,36 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class AppTest {
+
+    @Test
+    public void 테스트() {
+        assertTrue(1 == 1);
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void 스캐너에_키보드가_아닌_문자열을_입력으로_설정() {
+        Scanner sc = TestUtil.genScanner("안녕");
+
+        String cmd = sc.nextLine().trim();
+        assertEquals("안녕", cmd);
+    }
+
+    @Test
+    public void 출력을_모니터에_하지_않고_문자열로_얻기() {
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertEquals("안녕", rs);
+    }
 }
